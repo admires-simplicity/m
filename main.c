73de31f46bd2026de907f8f778cd3e5484ace321
Jesse <jc3124567@gmail.com>
2020-12-12 12:36:41 -0600

    big func

--- main.c
+++ main.c
@@ -25,10 +25,11 @@ enum key {
 };
 
 enum states {
-	OFF,
+	OFF = 1000,
 	ACTIVATED,
+	EMPTY,
 	FLAGGED,
-	SECONDARY_FLAGGED
+	MARKED
 };
 
 /*** DATA ***/
@@ -98,7 +99,7 @@ int b = 102;
 int minefield[16][30];
 int gamefield[16][30];
 
-int generation = 0;	//0 = random, 1 = friendly
+int generation = 1;	//0 = random, 1 = friendly
 
 /*** INIT ***/
 int main(void) {
@@ -197,7 +198,7 @@ void generate_minefield()
 void random_generate_minefield()
 {
 	int pos;		//for current position in loop
-	int bombs_placed;
+	int bombs_placed = 0;
 	int repeat = FALSE;
 	int position[b];
 
@@ -246,14 +247,224 @@ void random_generate_minefield()
 		die("NOT ENOUGH BOMBS!");
 }
 
-
+int coord_to_n(int y, int x)	//SHOULD THIS BE A MACRO?
+{
+	//I think this should be 0 indexed, so I'll build it that way, and if it needs to be 1 indexed i can change it later
+	return (w * y) + x;
+}
 
 void friendly_generate_minefield()
 {
-	//mask adjacent bombs
-	//randomgen
-	//unmask adjacent bombs
+	//int zzz = 0;
+
+	int pos;		//for current position in loop
+	int bombs_placed = 0;
+	int repeat = FALSE;
+	int position[b+9];
+
+	//initialize empty position array
+	for (int i = 0; i < b; ++i)
+		position[i] = 0;
+
+	//ADJACENT MASK
+	int x = 0;
+	minefield[T.cy][T.cx] = 1;
+	position[x] = coord_to_n(T.cy, T.cx);
+	++x;
+	if (              T.cy > 0  ) {
+		minefield[T.cy-1][T.cx  ] = 1;	// ABOVE
+		position[x] = coord_to_n(T.cy-1, T.cx);
+		++x;
+	}
+	if (T.cx < w-1 && T.cy > 0  ) {
+		minefield[T.cy-1][T.cx+1] = 1;	// ABOVE + RIGHT
+		position[x] = coord_to_n(T.cy-1, T.cx+1);
+		++x;
+	}
+	if (T.cx < w-1              ) {
+		minefield[T.cy  ][T.cx+1] = 1;	// RIGHT
+		position[x] = coord_to_n(T.cy, T.cx+1);
+		++x;
+	}
+	if (T.cx < w-1 && T.cy < h-1) {
+		minefield[T.cy+1][T.cx+1] = 1;	// DOWN + RIGHT 
+		position[x] = coord_to_n(T.cy+1, T.cx+1);
+		++x;
+	}
+	if (              T.cy < h-1) {
+		minefield[T.cy+1][T.cx  ] = 1;	// DOWN
+		position[x] = coord_to_n(T.cy+1, T.cx);
+		++x;
+	}
+	if (T.cx > 0   && T.cy < h-1) {
+		minefield[T.cy+1][T.cx-1] = 1;	// DOWN + LEFT
+		position[x] = coord_to_n(T.cy+1, T.cx-1);
+		++x;
+	}
+	if (T.cx > 0                ) {
+		minefield[T.cy  ][T.cx-1] = 1;	// LEFT
+		position[x] = coord_to_n(T.cy, T.cx-1);
+		++x;
+	}
+	if (T.cx > 0   && T.cy > 0  ) {
+		minefield[T.cy-1][T.cx-1] = 1;	// LEFT + UP
+		position[x] = coord_to_n(T.cy-1, T.cx-1);
+		++x;
+	}
+
+
+	//place req'd num bombs
+	while (bombs_placed < b) {
+		//random in acceptable range
+		pos = rand() % (w * h);
+
+		//check against array (could replace with binary tree for efficiency)
+		for (int i = 0; i < bombs_placed+x; ++i)
+			if (position[i] == pos) {
+				repeat = TRUE;
+				//++repeats;
+				break;
+			}
+
+		//continue if repeat found
+		if (repeat) {
+			repeat = FALSE;
+			continue;
+		}
+
+		//set pos in pos array
+		position[bombs_placed+x] = pos;	//this maybe should be in loop?
+
+		//set pos in minefield 2d array (could be way more efficient)
+		int i = 0;	//index for number of cells
+		for (int j = 0; j < h; ++j)
+			for (int k = 0; k < w; ++k) {
+				//printf("zzz = %d\r\n", zzz++);
+				if (i == pos) {
+					minefield[j][k] = 1; //bomb here
+					++bombs_placed;
+					k = w;
+					j = h;
+				/////	if (minefield[T.cy][T.cx] || adjacent_bombs(T.cy, T.cx)) { //if bomb is within radius of cursor
+				/////		minefield[j][k] = 0;
+				/////		--bombs_placed;
+				/////		position[bombs_placed] = 0;	//this maybe should be in loop?
+				/////	}
+				} else 
+					++i;
+			}
+	}
+
+	//ADJACENT UNMASK
+	minefield[T.cy][T.cx] = 0;
+	if (              T.cy > 0  ) minefield[T.cy-1][T.cx  ] = 0;	// ABOVE
+	if (T.cx < w-1 && T.cy > 0  ) minefield[T.cy-1][T.cx+1] = 0;	// ABOVE + RIGHT
+	if (T.cx < w-1              ) minefield[T.cy  ][T.cx+1] = 0;	// RIGHT
+	if (T.cx < w-1 && T.cy < h-1) minefield[T.cy+1][T.cx+1] = 0;	// DOWN + RIGHT 
+	if (              T.cy < h-1) minefield[T.cy+1][T.cx  ] = 0;	// DOWN
+	if (T.cx > 0   && T.cy < h-1) minefield[T.cy+1][T.cx-1] = 0;	// DOWN + LEFT
+	if (T.cx > 0                ) minefield[T.cy  ][T.cx-1] = 0;	// LEFT
+	if (T.cx > 0   && T.cy > 0  ) minefield[T.cy-1][T.cx-1] = 0;	// LEFT + UP
+
+	//after req'd num bombs finished, not enough bombs -> error
+	if (bombs_placed != b)
+		die("NOT ENOUGH BOMBS!");
+
+
+
+//	int zzz;
+//	int pos;
+//	zzz = 0;
+//
+//	int repeat = 0;
+//	int repeats = 0;
+//
+//	int bombs_placed = 0;
+//	int position[b];
+//	for (int i = 0; i < b; ++i)
+//		position[i] = 0;
+//	while (bombs_placed < b) {
+//		pos = rand() % (w * h);
+//
+//		//if (pos == 0)
+//		//	continue;
+//		for (int i = 0; i < bombs_placed; ++i)
+//			if (position[i] == pos) {
+//				repeat = 1;
+//				++repeats;
+//				break;
+//			}
+//
+//		if (repeat) {
+//			repeat = 0;
+//			continue;
+//		}
+//		
+//		int i = 0;
+//		for (int j = 0; j < h; ++j)
+//			for (int k = 0; k < w; ++k) {
+//				++zzz;
+//				if (zzz > 1000000) {
+//					for (int w = 0; w < bombs_placed+1; ++w)
+//						printf("position[%d] == %d", w, position[w]);
+//					printf("pos == %d", pos);
+//					printf("i==%d", i);
+//					printf(",bp==%d", bombs_placed);
+//					printf(",repeats==%d", repeats);
+//					die("probably error");
+//				}
+//				printf("%d\r\n", zzz);
+//				if (i == pos) {
+//					if (!(minefield[T.cy][T.cx] || adjacent_bombs(T.cy, T.cx))) { //if bomb is within radius of cursor
+//						minefield[j][k] = 1; //bomb here
+//						position[bombs_placed] = pos;
+//						++bombs_placed;
+//					}
+//					k = w; //continue
+//					j = h;
+//				} else
+//					++i;
+//			}
+//
+//
+//		//pos[44] == 0pos==187
+//		//pos[16] == 0pos[17] == 0pos==33
+//		//
+//	}
+//
+//
+//	printf("%d\r\n", zzz);
+//
+//	//the largest value of zzz should be 48960, should it not?
+//	//and i'm calculating repeat values, so, zzz should still not be over 48960
 }
+
+//attempt at friendly_generate that doesn't work because random_gen() doesn't count the positions beforehand because it shouldn't...
+//void friendly_generate_minefield()
+//{
+//	//mask adjacent bombs
+//	minefield[T.cy][T.cx] = 1;
+//	if (              T.cy > 0  ) minefield[T.cy-1][T.cx  ] = 1;	// ABOVE
+//	if (T.cx < w-1 && T.cy > 0  ) minefield[T.cy-1][T.cx+1] = 1;	// ABOVE + RIGHT
+//	if (T.cx < w-1              ) minefield[T.cy  ][T.cx+1] = 1;	// RIGHT
+//	if (T.cx < w-1 && T.cy < h-1) minefield[T.cy+1][T.cx+1] = 1;	// DOWN + RIGHT 
+//	if (              T.cy < h-1) minefield[T.cy+1][T.cx  ] = 1;	// DOWN
+//	if (T.cx > 0   && T.cy < h-1) minefield[T.cy+1][T.cx-1] = 1;	// DOWN + LEFT
+//	if (T.cx > 0                ) minefield[T.cy  ][T.cx-1] = 1;	// LEFT
+//	if (T.cx > 0   && T.cy > 0  ) minefield[T.cy-1][T.cx-1] = 1;	// LEFT + UP
+//	//randomgen
+//	random_generate_minefield();
+//	//unmask adjacent bombs
+//	if (              T.cy > 0  ) minefield[T.cy-1][T.cx  ] = 0;	// ABOVE
+//	if (T.cx < w-1 && T.cy > 0  ) minefield[T.cy-1][T.cx+1] = 0;	// ABOVE + RIGHT
+//	if (T.cx < w-1              ) minefield[T.cy  ][T.cx+1] = 0;	// RIGHT
+//	if (T.cx < w-1 && T.cy < h-1) minefield[T.cy+1][T.cx+1] = 0;	// DOWN + RIGHT 
+//	if (              T.cy < h-1) minefield[T.cy+1][T.cx  ] = 0;	// DOWN
+//	if (T.cx > 0   && T.cy < h-1) minefield[T.cy+1][T.cx-1] = 0;	// DOWN + LEFT
+//	if (T.cx > 0                ) minefield[T.cy  ][T.cx-1] = 0;	// LEFT
+//	if (T.cx > 0   && T.cy > 0  ) minefield[T.cy-1][T.cx-1] = 0;	// LEFT + UP
+//	minefield[T.cy][T.cx] = 0;
+//}
 
 
 //////THIS DOESN'T WORK YET?
@@ -350,6 +561,14 @@ void friendly_generate_minefield()
 ////	//and i'm calculating repeat values, so, zzz should still not be over 48960
 ////}
 
+void initialize_gamefield()
+{
+	int i, j;
+
+	for (i = 0; i < h; ++i)
+		for (j = 0; j < w; ++j)
+			gamefield[i][j] = OFF;
+}
 
 
 //initialize minefields in T struct
@@ -360,6 +579,7 @@ void init_editor() {
 	srand(time(NULL));
 
 	initialize_minefield();
+	initialize_gamefield();
 
 	if (get_win_size(&T.screenrows, &T.screencols) == -1) die("get_win_size");
 }
@@ -530,15 +750,55 @@ void draw_minefield(struct abuf *ab)
 			if (minefield[i][j] == 1)
 				abAppend(ab, "*", 1);
 			else
-		//		if (z = adjacent_bombs(i, j)) {	//not 0 adj boms
-		//			snprintf(buf, sizeof(buf), "%d", z);
-		//			abAppend(ab, buf, strlen(buf));
-		//		} else {
+				if (z = adjacent_bombs(i, j)) {	//not 0 adj boms
+					snprintf(buf, sizeof(buf), "%d", z);
+					abAppend(ab, buf, strlen(buf));
+				} else {
 					abAppend(ab, ".", 1);
-		//		}
+				}
 		}
 		abAppend(ab, "\r\n", 2);
 	}
+}
+
+void draw_gamefield(struct abuf *ab)
+{
+	char *c;
+	char buf[2]; //size for one char and null byte			//AT SOME POINT THIS SHOULD BE REPLACED WITH A FUNCTION TO WRITE CHARS TO SCREEN
+	int z;
+
+	for (int i = 0; i < h; ++i) {
+		for (int j = 0; j < w; ++j) {
+			switch (gamefield[i][j]) {
+				case OFF:
+					abAppend(ab, "#", 1);
+					break;
+				case ACTIVATED:
+					abAppend(ab, "*", 1);
+					break;
+				case EMPTY:
+					abAppend(ab, ".", 1);
+					break;
+				case FLAGGED:
+					abAppend(ab, "!", 1);
+					break;
+				case MARKED:
+					abAppend(ab, "?", 1);
+					break;
+				case 0:
+					abAppend(ab, "#", 1);
+					break;
+				default:
+					buf[0] = gamefield[i][j] + '0';
+					buf[1] = '\0';
+					abAppend(ab, buf, 1);
+					break;
+			}
+		}
+		abAppend(ab, "\r\n", 2);
+	}
+
+
 }
 
 int count_bombs()
@@ -558,7 +818,8 @@ void refresh_screen() {
 	abAppend(&ab, "\x1b[H", 3);
 
 	//draw_rows(&ab);
-	draw_minefield(&ab);
+	//draw_minefield(&ab);
+	draw_gamefield(&ab);
 	char bug[20];
 	snprintf(bug, sizeof(bug), "nbombs == %d\r\n", count_bombs());
 	abAppend(&ab, bug, strlen(bug));
@@ -571,6 +832,30 @@ void refresh_screen() {
 
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
+}
+
+////THIS FUNCTION SHOULD NOT GO HERE BUT I DON'T KNOW WHERE TO PUT IT 
+void step()
+{
+	int i = 0;
+
+	if (minefield[T.cy][T.cx])
+		//gameover();
+		die("gameover!");
+	else if (i = adjacent_bombs(T.cy, T.cx))
+		gamefield[T.cy][T.cx] = i;
+	else
+		gamefield[T.cy][T.cx] = EMPTY;
+}
+
+void flag_coord()
+{
+	gamefield[T.cy][T.cx] = FLAGGED;
+}
+
+void mark_coord()
+{
+	gamefield[T.cy][T.cx] = MARKED;
 }
 
 /*** INPUT ***/
@@ -639,6 +924,18 @@ void process_keypress() {
 
 		case CTRL_KEY('r'):
 			initialize_minefield();
+			break;
+
+		case ' ':
+			step();
+			break;
+
+		case 'f':
+			flag_coord();
+			break;
+
+		case 'd':
+			mark_coord();
 			break;
 
 		default:
